a4abfeb64702e45306d2a763a03fd9d830877ee8
Fanny <fanny_schriwer@hotmail.com>
2018-11-23 13:41:16 +0100

    added database connection

---
+++ includes/database_connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$pdo = new PDO(
+      "mysql:host=localhost;port=8889;dbname=millhouse;charset=utf8",
+      "root",
+      "root"
+);
+
+//this shows PDO errors
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//fetching as associative array
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
