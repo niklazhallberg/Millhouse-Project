13ff483d54dd95ec3cbb8ccf550d9fc28990cc09
kevinjonsson <kevin.jonsson92@gmail.com>
2018-11-28 15:02:18 +0100

    insert form input to database

--- includes/add_form.php
+++ includes/add_form.php
@@ -1,7 +1,6 @@
-<form class="add-post" action="../includes/add_post_sql.php" method="post">
+<form class="add-post" action="../includes/add_post_sql.php" method="POST" enctype=“multipart/form-data”>
   <input type="text" name="title" placeholder="heading"><br>
-  <div class="text-area-post">
     <textarea name="description" rows="10" cols="60"></textarea><br>
-  </div>
-  <input type="submit" name="Add" value="Add post">
+  <input type="file" name="image" id="image">
+  <input type="submit" value="Send">
 </form>
--- includes/add_post_sql.php
+++ includes/add_post_sql.php
@@ -6,16 +6,25 @@ include 'database_connection.php';
 $blog_title = $_POST["title"];
 $blog_description = $_POST["description"];
 $user_id = $_SESSION["user_id"];
+$image = $_FILES["image"];
 
-//
-$statement = $pdo->prepare("INSERT INTO (title, description, created_by) VALUES (:title, :description, :created_by)");
+
+$temporary_location = $image["tmp_name"];
+
+$new_location = "images/" . $image["name"];
+
+$upload_ok = move_uploaded_file($temporary_location, $new_location);
+
+if($upload_ok){
+$statement = $pdo->prepare("INSERT INTO posts (title, description, created_by, image) VALUES (:title, :description, :created_by, :image)");
 $statement->execute(
   [
     ":title" => $blog_title,
     ":description" => $blog_description,
-    ":created_by" => $user_id
+    ":created_by" => $user_id,
+    ":image" => $new_location
   ]
 );
 // redirect
 header('location: ../views/add_post.php');
-?>
+}
