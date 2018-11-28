fd0c73f50dd9aa049c76410d533cdc59009ca76f
niklazhallberg <niklaz.a.hallberg@gmail.com>
2018-11-28 13:53:16 +0100

    updated loop in index_sidebar_foreach_recent_post.php

--- includes/index_sidebar_foreach_recent_post.php
+++ includes/index_sidebar_foreach_recent_post.php
@@ -1,9 +1,12 @@
 <?php
 
+$i = 0;
+
 foreach($posts as $post): ?>
 
               <li><a href="../views/single_post.php?post_id=<?=$post["id"]?>"> <?=$post["title"]?></a></li>
 	<?php
+
 	$i++;
 	if($i==10) break;
 	?>
