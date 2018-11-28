57f5ec5a5bdfd79d51deace7ba49cde6ae468c12
kevinjonsson <kevin.jonsson92@gmail.com>
2018-11-28 10:39:08 +0100

    started add_post

---
+++ includes/add_form.php
@@ -0,0 +1,5 @@
+<form class="add-post" action="../includes/add_form.php" method="post">
+  <input type="text" name="rubrik" value="rubrik">
+  <input type="textarea" name="blogg post" value="blogg post">
+  <input type="submit" name="Add" value="Add post">
+</form>
