93b7976575880da8c2e5a46492807e93484a357f
kevinjonsson <kevin.jonsson92@gmail.com>
2018-12-03 13:35:33 +0100

    small fixes

--- classes/call.php
+++ classes/call.php
@@ -1,5 +1,4 @@
-<?php 
-session_start();
+<?php
 
 include 'db_con.php';
 include 'User.php';
--- classes/db_con.php
+++ classes/db_con.php
@@ -1,14 +1,11 @@
-<?php 
-
-session_start();
-
+<?php
 
 try
 {
-     $pdo = new PDO("mysql:host=localhost;dbname=millhouse;charset=utf8","root","root");
-     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo = new PDO("mysql:host=localhost;dbname=millhouse;charset=utf8","root","root");
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $error)
 {
-     echo $error->getMessage();
+  echo $error->getMessage();
 }
