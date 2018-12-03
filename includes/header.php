0bd1c91d2c8c80e21947ee70f47a0aaad07320bb
therese.rossi <therese.rossi@mail.com>
2018-12-03 21:20:29 +0100

    Some basic styling to index and header

--- includes/header.php
+++ includes/header.php
@@ -1,24 +1,22 @@
 <header>
-  <div class="row justify-content-center mr-0 ml-0">
-    <div class="col-sm-6 col-md-6 col-lg-4 header-logo">
+  <div class="row header justify-content-end align-items-center mr-0 ml-0">
+    <div class="header-logo">
       <a href="../index.php"><img src="../images/logo_dark.png"  alt="Millhouse logo"></a>
     </div>
-  </div>
-    <div class="row">
-      <div class="col-8">
-        <?php
 
-
-        if($user->isAdmin()){
-          echo "Congratulations! You have special privileges. Use them wisely.";
-        }
-        ?>
-        </div>
-        <div class="col-4 logout-button">
-          <?php
-          if($user->isLoggedIn()) {
+    <div class="header-buttons">
+      <div>
+        <?php if($user->isLoggedIn()) {
             include 'logout_button.php';
         } ?>
       </div>
+
+      <div>
+        <?php if($user->isAdmin()){
+          include 'add_post_button.php';
+        } ?>
+      </div>
+
     </div>
+  </div>
 </header>
--- includes/logout_button.php
+++ includes/logout_button.php
@@ -1,1 +1,5 @@
-<a href="../includes/logout_sql.php">Sign out <i class="fas fa-sign-out-alt"></i></a>
+
+<button class="button"><a href="../includes/logout_sql.php"><span>Logout </span></a></button>
+
+
+<!-- <a href="../includes/logout_sql.php">Sign out <i class="fas fa-sign-out-alt"></i></a> -->
