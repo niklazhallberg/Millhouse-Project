2aee8f95a4e6911aefee410d1a1d825446b3fe28
niklazhallberg <niklaz.a.hallberg@gmail.com>
2018-12-03 21:56:52 +0100

    Implemented OOP classes and functions to blogpost-section and all categories in sidebar

--- includes/index_sidebar_foreach_furnishing_post.php
+++ includes/index_sidebar_foreach_furnishing_post.php
@@ -1,24 +1,9 @@
 <?php
 
- $statement = $pdo->prepare("SELECT * FROM posts WHERE category_id = '3'");
-
-    $statement->execute();
-
-    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-$i = 0;
-
-foreach($posts as $post):?>
+foreach($furnishing_posts as $post):?>
 
               <li>
               	<a href="../views/single_post.php?post_id=<?=$post["id"]?>"><?=$post["title"]?></a>
               </li>
-
-          <?php
-
-	$i++;
-	if($i==10) break;
-
-	?>
 
 <?php endforeach;?>
--- index.php
+++ index.php
@@ -2,26 +2,28 @@
 session_start();
 include 'classes/call.php';
 if(!$user->isLoggedIn()) {
-    $user->redirect('views/login.php');
+  $user->redirect('views/login.php');
 }
 
 if(isset($_GET["delete_post"])){
-    $post_id = $_GET["delete_post"];
-    $delete_post = $posts->deletePostWithId($post_id);
+  $post_id = $_GET["delete_post"];
+  $delete_post = $posts->deletePostWithId($post_id);
 }
 ?>
 
- <?php include 'includes/head.php'; ?>
+<?php include 'includes/head.php'; ?>
 <body>
   <?php include 'includes/header.php'; ?>
 
   <main class="container-fluid">
-  <div class="row">
+    <div class="row">
      <div class="col-12">
        <div class="row">
          <section class="col-12 col-md-9 blogpost-section">
-            
+
            <?php 
+
+           /* NUMBER OF POSTS DISPLAYED IN MAIN BLOG */
 
            $number_of_posts = 5;
            $index_posts = $posts->getLatestPosts($number_of_posts);
@@ -29,40 +31,61 @@ if(isset($_GET["delete_post"])){
 
          </section>
 
-            <aside class="col-12 col-md-3 index-sidebar">
+         <!-- ASIDE SECTION WITH ARTICLE CATERGORIES --> 
 
-            <?php if($user->isAdmin()) {
+         <aside class="col-12 col-md-3 index-sidebar">
+
+          <?php if($user->isAdmin()) {
             include 'includes/go_to_add_page_button.php';
-              }?>
-              
-            <h3>Recent</h3>
-            <ul>
+          }?>
+
+          <h3>Recent</h3>
+          <ul>
             <?php   
+
+            /* NUMBER OF RECENT POSTS DISPLAYED IN SIDEBAR */ 
 
             $number_of_posts = 10;
             $aside_posts = $posts->getLatestPosts($number_of_posts);
             include 'includes/index_sidebar_foreach_recent_post.php'; ?>
 
-            </ul>
-            <h3>Watches</h3>
-            <ul>
+          </ul>
+          <h3>Watches</h3>
+          <ul>
+            <?php 
 
-                <?php $number_of_watch_posts = 10;
-                  $watch_posts = $posts->getLatestPosts($number_of_posts);
-                  include 'includes/index_sidebar_foreach_watch_post.php'; ?>
+            /* NUMBER OF WATCH POSTS DISPLAYED IN SIDEBAR */ 
 
-            </ul>
-            <h3>Sunglasses</h3>
-            <ul>
-              <?php include 'includes/index_sidebar_foreach_sunglasses_post.php'; ?>
-            </ul>
-            <h3>Furnishing articles</h3>
-            <ul>
-             <?php include 'includes/index_sidebar_foreach_furnishing_post.php'; ?>
-            </ul>
-          </aside>
-       </div>
-     </div>
+            $number_of_watch_posts = 10;
+            $watch_posts = $posts->getLatestWatchPosts($number_of_watch_posts);
+            include 'includes/index_sidebar_foreach_watch_post.php'; ?>
+
+          </ul>
+          <h3>Sunglasses</h3>
+          <ul>
+            <?php 
+
+            /* NUMBER OF SUNGLASSES POSTS DISPLAYED IN SIDEBAR */ 
+
+            $number_of_sunglasses_posts = 10;
+            $sunglasses_posts = $posts->getLatestSunglassesPosts($number_of_sunglasses_posts);
+            include 'includes/index_sidebar_foreach_sunglasses_post.php'; ?>
+
+          </ul>
+          <h3>Furnishing articles</h3>
+          <ul>
+            <?php 
+
+            /* NUMBER OF FURNISHING POSTS DISPLAYED IN SIDEBAR */
+
+            $number_of_furnishing_posts = 10;
+            $furnishing_posts = $posts->getLatestFurnishingPosts($number_of_furnishing_posts);
+            include 'includes/index_sidebar_foreach_furnishing_post.php'; ?>
+
+          </ul>
+        </aside>
+      </div>
+    </div>
   </div>
 </main>
 
