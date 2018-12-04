5c0fce308088d67f20f19d62e005a8205cd09854
Fanny <fanny_schriwer@hotmail.com>
2018-12-04 15:43:17 +0100

    some sort of commit

--- classes/User.php
+++ classes/User.php
@@ -30,7 +30,7 @@ class User
        {
            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
    
-           $statement = $this->pdo->prepare("INSERT INTO users (username, password, email, first_name, last_name, date_of_birth) VALUES (:username, :password, :email, :first_name, :last_name, :date_of_birth, :is_admin)");            
+           $statement = $this->pdo->prepare("INSERT INTO users (username, password, email, first_name, last_name, date_of_birth, is_admin) VALUES (:username, :password, :email, :first_name, :last_name, :date_of_birth, :is_admin)");            
            $statement->execute([":username" => $username, ":password" => $hashed_password, ":email" => $email, ":first_name" => $first_name, ":last_name" => $last_name, ":date_of_birth" => $date_of_birth, ":is_admin" => 1]);
         
            return $statement; 
