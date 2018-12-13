026b9b232e90fa31f2076f57bbf99c76a6675ac9
therese.rossi <therese.rossi@mail.com>
2018-12-13 10:43:40 +0100

    missed register thing added

--- classes/User.php
+++ classes/User.php
@@ -15,7 +15,7 @@ class User
         //inserts the password as hashed into database for security
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-        $statement = $this->pdo->prepare("INSERT INTO users (username, password, email, first_name, last_name, date_of_birth, is_admin) VALUES (:username, :password, :email, :first_name, :last_name, :date_of_birth)");            
+        $statement = $this->pdo->prepare("INSERT INTO users (username, password, email, first_name, last_name, date_of_birth) VALUES (:username, :password, :email, :first_name, :last_name, :date_of_birth)");            
         $statement->execute([":username" => $username, ":password" => $hashed_password, ":email" => $email, ":first_name" => $first_name, ":last_name" => $last_name, ":date_of_birth" => $date_of_birth]);
     
         return $statement; 
